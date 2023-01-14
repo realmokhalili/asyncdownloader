f6225ab8e830d428659ea563267635dfe1ab123c
Mohammad Khalili <immam3d@gmail.com>
2023-01-14 13:04:24 +0330

    feat: add AioHTTP class

--- http.py
+++ http.py
@@ -1,3 +1,4 @@
+import aiohttp
 from abc import ABC, abstractmethod
 
 class BaseHTTP(ABC):
@@ -8,3 +9,16 @@ class BaseHTTP(ABC):
     @abstractmethod
     async def head(self, url):
         pass
+
+
+class AioHTTP(BaseHTTP):
+    def __init__(self, client):
+        self.client: aiohttp.ClientSession = client
+
+    async def get(self, url):
+        async with self.client.get(url) as response:
+            return response
+
+    async def head(self, url):
+        async with self.client.head(url) as response:
+            return response
